50c577ded6a574e50177b977195928bc389073b1
Parveen Yadav <parveeenrao7727@gmail.com>
2025-07-18 21:53:18 +0530

    second  version of data saved

--- my_code.py
+++ my_code.py
@@ -13,6 +13,12 @@ data = {'Name':['Parveen' , 'Savita' , 'Priyanka' , 'Nirmala'],
 
 df = pd.DataFrame(data)
 
+ # Adding new row to df for V2
+new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
+df.loc[len(df.index)] = new_row_loc
+
+
+
 data_dir = 'data'
 
 os.makedirs(data_dir , exist_ok=True)
